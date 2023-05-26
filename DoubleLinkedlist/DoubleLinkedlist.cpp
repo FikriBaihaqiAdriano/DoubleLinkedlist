097d59b0d7180e2b6f436f79198da16e1b33b43c
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-05-26 15:46:44 +0700

    membuat program void yang berisi int nim dan string nm

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -23,5 +23,13 @@ public:
 	void revtraverse();
 	void hapus();
 	void searchData();
-
 };
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+}
